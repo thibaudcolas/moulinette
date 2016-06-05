01b06d793dfe0ac29c324cc30a09057ba6fe4a4f
Thibaud Colas <thibaudcolas@gmail.com>
2016-06-05 18:35:14 +0300

    Save residues to disk, as input for the next script

--- PDB/pymol/interfaceResidues.py
+++ PDB/pymol/interfaceResidues.py
@@ -3,6 +3,23 @@ from pymol import cmd, stored
 PDB_CODES_FILE = 'pdb-codes.txt'
 
 
+def read_pdb_codes():
+    return [line.rstrip('\n') for line in open(PDB_CODES_FILE)]
+
+
+pdb_codes = read_pdb_codes()
+
+
+def fetch_pdb_files():
+    for code in pdb_codes:
+        success = cmd.fetch(code)
+        print 'Fetched ' + success
+
+
+def load_pdb_files():
+    for code in pdb_codes:
+        filename = code + '.pdb'
+        cmd.load(filename, code)
 
 
 def interfaceResidues(cmpx, cA='c. A', cB='c. B', cutoff=1.0, selName="interface"):
@@ -117,31 +134,29 @@ def interfaceResidues(cmpx, cA='c. A', cB='c. B', cutoff=1.0, selName="interface
 
     return rVal
 
-# cmd.extend("interfaceResidues", interfaceResidues)
 
+def save_residues():
+    for pdb_code in pdb_codes:
+        selName = pdb_code + '-interface'
+        print pdb_code
+        interfaceResidues(pdb_code, selName=selName)
+        space = {
+            'residues': []
+        }
+        # There probably is a simpler way to do this using idiomatic Python,
+        # but I don't know enough PyMOL to understand what is going on here.
+        # cmd.iterate(selName + ' & n. ca', 'print chain,resi,resn')
+        selection = selName + ' & n. ca'
+        expression = 'residues.append((chain, resi, resn))'
+        cmd.iterate(selection, expression, space=space)
 
+        file = open('residues/' + pdb_code + '.txt', 'w')
+        # print space['residues']
+        for residue in space['residues']:
+            file.write(' '.join(residue) + '\n')
 
-
-
-
-def read_pdb_codes():
-    return [line.rstrip('\n') for line in open(PDB_CODES_FILE)]
-
-
-pdb_codes = read_pdb_codes()
-
-
-def fetch_pdb_files():
-    for code in pdb_codes:
-        success = cmd.fetch(code)
-        print 'Fetched ' + success
-
-
-def load_pdb_files():
-    for code in pdb_codes:
-        filename = code + '.pdb'
-        cmd.load(filename, code)
-
+        file.close()
 
 fetch_pdb_files()
 load_pdb_files()
+save_residues()
