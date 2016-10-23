a0e2bdf577061e918098f5046efac6379aa96169
Thibaud Colas <thibaudcolas@gmail.com>
2016-10-23 14:59:53 +0300

    Update install script

--- bin/pymol-install.sh
+++ bin/pymol-install.sh
@@ -2,33 +2,18 @@
 prefix=$PWD/pymol
 modules=$prefix/modules
 svnpymol=svnpymol
-update=$prefix/updatepymol.sh
 
 ###################################################
 mkdir -p $prefix
 mkdir -p $HOME/bin
 
 ###### Checkout pymol svn
-svn co svn://svn.code.sf.net/p/pymol/code/trunk/pymol $prefix/$svnpymol
+svn co --quiet svn://svn.code.sf.net/p/pymol/code/trunk/pymol $prefix/$svnpymol
 ###### Build and install pymol
 cd $prefix/$svnpymol
 python setup.py build install --home=$prefix --install-lib=$modules --install-scripts=$prefix
 
-## Make a update script
-cat > "$update" <<EOF
-#!/bin/bash -e
-prefix=$prefix
-modules=$modules
-svnpymol=svnpymol
-
-###### Checkout pymol svn
-svn co svn://svn.code.sf.net/p/pymol/code/trunk/pymol \$prefix/\$svnpymol
-###### Build and install pymol
-cd \$prefix/\$svnpymol
-python setup.py build install --home=\$prefix --install-lib=\$modules --install-scripts=\$prefix
-EOF
-chmod +x $update
-
-alias pymol="$prefix/pymol"
-ls "$prefix"
+export PATH="$prefix:$PATH"
 export PYMOL_PATH="$modules"
+ls "$prefix"
+ls "$modules"
